55bfe26995ef037343df25671ec3f3ff103bdfd4
unknown <ivezicljubisa@gmail.com>
2019-09-23 20:13:48 +0200

    env development

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -7,8 +7,8 @@ export default class Recipes extends Component {
   state = {
     recipes: [],
     search: "",
-    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
-    base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_API_KEY}`,
+    base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_API_KEY}`,
     query: "&q=",
     еrror: ""
   };
